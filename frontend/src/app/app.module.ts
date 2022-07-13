95444fe1ee0afd8868a52179c5be1ae1b3795ed5
jenikaretnakaran <jenikav952@gmail.com>
2022-07-13 22:22:35 +0530

    adminpage created

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminDashboardComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
