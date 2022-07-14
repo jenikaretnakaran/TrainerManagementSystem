d65b80a0978f6e15c476401ba1e9366190427391
jenikaretnakaran <jenikav952@gmail.com>
2022-07-14 22:13:20 +0530

    merge conflicts

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,10 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 import { AdminComponent } from './admin/admin.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 const routes: Routes = [
   {path:"" , component:HomeComponent},
+  {path:"",component:HeaderComponent},
+  {path:"",component:FooterComponent},
   {path:"admin",component:AdminComponent,
   children:[
     {path:"dashboard",component:AdminDashboardComponent}
