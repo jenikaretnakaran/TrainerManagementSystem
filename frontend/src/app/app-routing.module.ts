d8b397df8e22692c9c9069911011253268aac584
akhila-baiju <akhilabaijuravi@gmail.com>
2022-07-14 15:12:02 +0530

    Trainer Created

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
-
+import { TrainerHomeComponent } from './trainer-home/trainer-home.component';
+import { TrainerEnrollmentComponent } from './trainer-enrollment/trainer-enrollment.component';
+import { TrainerComponent } from './trainer/trainer.component';
+import { TrainerWaitingComponent } from './trainer-waiting/trainer-waiting.component';
+const routes: Routes = [
+                    {path:'',component:TrainerHomeComponent},
+                    {path:'trainer',component:TrainerComponent},
+                    {path:'enrollment',component:TrainerEnrollmentComponent},
+                    {path:'waiting',component:TrainerWaitingComponent}];
+                  /*  children:[
+                          {path:' ',component:TrainerHomeComponent},
+                          {path:'enrollment',component:TrainerEnrollmentComponent}
+                          ]}];*/
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
---
+++ frontend/src/app/trainer/trainer.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-trainer',
+  templateUrl: './trainer.component.html',
+  styleUrls: ['./trainer.component.css']
+})
+export class TrainerComponent implements OnInit {
+
+  constructor(public route:Router) { }
+  title:string="ENROLLMENT FORM"
+  ngOnInit(): void {
+  }
+  courses=[
+    "CERTIFIED XR ASSOCIATE","CERTIFIED CYBER SECURITY ANALYST","CERTIFIED SPECIALIST IN FULL STACK DEVELOPMENT",
+    "CERTIFIED SPECIALIST INs DATA SCIENCE & ANALYTICS","MICRO SKILLS TRAINING ON ROBOTIC PROCESS AUTOMATION",
+    "MICRO SKILLS TRAINING ON DIGITAL MARKETING AND SEO","CERTIFIED SPECIALIST IN MACHINE LEARNING AND ARTIFICIAL INTELLIGENCE",
+    "MOODLE","ARM EMBEDDED SYSTEMS","IOT FOR ENGINEERING APPLICATIONS","AWS EDUCATE"
+  ]
+  fname="";
+  lname="";
+  address="";
+  email:any=localStorage.getItem('email');
+  phno="";
+  qual="";
+  skill="";
+  comp="";
+  desgn="";
+  course="";
+  img:any;
+  url:any="./assets/images/trainer.jpg"
+  onFileSelected(event:any){
+    if(event.target.files){
+      var reader=new FileReader();
+      reader.readAsDataURL(event.target.files[0]);
+      reader.onload=(e:any)=>{
+        this.url=e.target.result;
+      }
+      this.img=event.target.files[0];
+    }
+    
+  }
+
+  addTrainer()
+  {
+    this.route.navigate(['waiting']);
+  }
+
+  validate()
+  {
+    alert("submit");
+    console.log("subited");
+  }
+}
