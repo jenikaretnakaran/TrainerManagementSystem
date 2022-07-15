5e8e2b43c32912d0f5980f1f6b5200dba0883592
akhila-baiju <akhilabaijuravi@gmail.com>
2022-07-15 14:11:28 +0530

    trainer page added

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TrainerHomeComponent } from './trainer-home/trainer-home.component';
+import { TrainerEnrollmentComponent } from './trainer-enrollment/trainer-enrollment.component';
 
-const routes: Routes = [];
-
+import { TrainerWaitingComponent } from './trainer-waiting/trainer-waiting.component';
+const routes: Routes = [
+                    {path:'',component:TrainerHomeComponent},
+                   
+                    {path:'enrollment',component:TrainerEnrollmentComponent},
+                    {path:'waiting',component:TrainerWaitingComponent}];
+                  
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TrainerHomeComponent } from './trainer-home/trainer-home.component';
+import { TrainerEnrollmentComponent } from './trainer-enrollment/trainer-enrollment.component';
+
+import { TrainerWaitingComponent } from './trainer-waiting/trainer-waiting.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TrainerHomeComponent,
+    TrainerEnrollmentComponent,
+   
+    TrainerWaitingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
