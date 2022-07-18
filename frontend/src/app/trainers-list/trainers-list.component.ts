7c92e6d44183585e13745152f7af767f1cf388c5
jenikaretnakaran <jenikav952@gmail.com>
2022-07-18 10:48:42 +0530

    frontend update

--- frontend/src/app/trainers-list/trainers-list.component.ts
+++ frontend/src/app/trainers-list/trainers-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AdminService } from '../admin.service';
 
 @Component({
   selector: 'app-trainers-list',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TrainersListComponent implements OnInit {
 
-  constructor() { }
+  trainerData=[{
+    trainerName:"",
+    email:"",
+    typeOfEmp:"",
+    course:""
+  }]
+  constructor(private adminservice:AdminService,private router:Router) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void 
+  {
+    this.adminservice.getTrainersList().subscribe((data)=>{
+      this.trainerData=JSON.parse(JSON.stringify(data));
+    })
   }
 
+  allocate(trainer){
+    localStorage.setItem("getTrainerId",trainer._id.tostring());
+    localStorage.setItem('mail',trainer.email);
+    this.router.navigate(["/admin"]) ///edit
+  }
+  removeTrainer(trainer){
+    this.adminservice.removeTrainer(trainer._id)
+    .subscribe((data)=>{
+      this.trainerData=this.trainerData.filter(t => t !== trainer)
+    })
+  }
 }
