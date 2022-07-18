2f1a1116775a1b1a5026cc9fbb2ac96374c463ec
akhila-baiju <akhilabaijuravi@gmail.com>
2022-07-18 15:09:57 +0530

    tTainer profile added

---
+++ frontend/src/app/trainer-enrollment/trainer.model.ts
@@ -0,0 +1,16 @@
+export class TrainerModel{
+    constructor(
+        public name:String,
+        public email: String,
+        public phone:Number,
+        public address:String,
+        public qualification: String,
+        public skill:String,
+        public company:String,
+        public designation: String,
+        public course:String,
+        public trainerURL:String        
+    ){}
+}
+
+
