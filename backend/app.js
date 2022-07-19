f396b625d8804bcf87fac553f810fd6752443ec1
jenikaretnakaran <jenikav952@gmail.com>
2022-07-19 10:36:46 +0530

    routes added

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,30 @@
 const express = require("express");
 const app = express();
+const mongoose= require('mongoose');
+const cors=require("cors");
+// const path = require('path');
+
+const adminRoutes =require("./routes/admin")
 
 const PORT = process.env.PORT || 3000;
 
+//DB Connection
+mongoose
+  .connect("mongodb://localhost:27017/project",{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB CONNECTED");
+  });
+
+  //Routes
+  app.use("/api", adminRoutes);
+ 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 
 app.get("/" , (req,res)=>{
