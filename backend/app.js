5cd4f8e8097c4e7eda955f186d7fd99bfe3d1d33
jenikaretnakaran <jenikav952@gmail.com>
2022-07-24 12:52:58 +0530

    errors rechecked

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 3000;
 // "mongodb://localhost:27017/project"
 
 mongoose
-  .connect("mongodb://localhost:27017/project" ,{
+  .connect("mongodb+srv://admin:user123@project1.cfkyt.mongodb.net/ICTAK?retryWrites=true&w=majority",{
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
